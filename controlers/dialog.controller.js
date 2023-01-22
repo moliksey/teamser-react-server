398de58395ee99cb8a555ecd0e33443fd3d67932
moliksey <moliksey@yandex.ru>
2023-01-22 16:50:58 +0300

    dialog fixes

--- controlers/dialog.controller.js
+++ controlers/dialog.controller.js
@@ -12,8 +12,25 @@ class DialogController {
 
     async getUsersDialogs(req, res) {
         const decoded = jwt.verify(req.headers.authorization.split(' ')[1], SECRET_WORD)
-        const user = await db.query('SELECT * FROM t_dialogs where user1_id=$1 or user2_id=$1', [decoded.userId]);
-        res.json(user.rows);
+        const dialog = await db.query('SELECT * FROM t_dialogs where user1_id=$1 or user2_id=$1', [decoded.userId]);
+        let index=0;
+        let ans=[];
+        for (index = 0; index < dialog.rows.length; ++index) {
+            let op;
+            if(dialog.rows[index].user1_id==decoded.userId)
+                op= await db.query('SELECT * FROM t_user where id=$1', [dialog.rows[index].user2_id]);
+            else
+                op = await db.query('SELECT * FROM t_user where id=$1', [dialog.rows[index].user1_id]);
+            let lastMessage=await db.query('SELECT * FROM t_messages where dialog_id=$1 order by date DESC', [dialog.rows[index].id]);
+            let ansStruct={
+                id:dialog.rows[index].id,
+                opname:op.rows[0].username,
+                date: lastMessage.rows[0].date,
+                text: lastMessage.rows[0].text
+            }
+            ans.push(ansStruct);
+        }
+        res.json(ans);
     }
 
     async deleteDialog(req, res) {
